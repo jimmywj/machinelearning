6d0344a889f9d8815868bc53942ecee0c31e8582
Jimmy Wong <jimmywj@gmail.com>
2017-11-21 10:08:40 +0800

    separate folder for tensorflow learning

---
+++ tf/boxbin/boxbin.py
@@ -0,0 +1,4 @@
+# This module is used to detect the box and bin in the photos
+# Box and Bin is a storage device used to hold the cargo pieces in the normal cargo terminal handing.
+#
+import tensorflow as tf
