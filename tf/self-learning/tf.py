943809b7df19d928bea1d5bbd746635560b24214
Jimmy Wong <jimmywj@gmail.com>
2017-11-21 12:38:49 +0800

    completed get-started

--- tf/self-learning/tf.py
+++ tf/self-learning/tf.py
@@ -12,3 +12,53 @@ print(sess.run([node1, node2]))
 node3 = tf.add(node1, node2)
 print("node3:", node3)
 print("sess.run(node3):", sess.run(node3))
+
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b  # + provides a shortcut for tf.add(a, b)
+
+
+print(sess.run(adder_node, {a: 3, b: 4.5}))
+print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
+
+
+add_and_triple = adder_node * 3.
+print(sess.run(add_and_triple, {a: 3, b: 4.5}))
+print(sess.run(add_and_triple, {a: [9,13], b: [4.5,7]}))
+
+#
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W*x + b
+init = tf.global_variables_initializer()
+sess.run(init)
+#print(sess.run(linear_model, {a: 3, b: 4.5, W:1, x:2}))
+print(sess.run(linear_model, {x: [1,2,3,4]}))
+
+y = tf.placeholder(tf.float32)
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+
+# change to guess the weight and bias of the function neuron?
+fixW = tf.assign(W, [-1.])
+fixb = tf.assign(b, [1.])
+sess.run([fixW, fixb])
+print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+
+# use the optimizer to guess the value of W and b
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
+
+sess.run(init) # reset values to incorrect defaults.
+for i in range(1000):
+  sess.run(train, {x:  x_train, y: y_train})
+
+curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x:  x_train, y: y_train})
+
+print("W: %s, b: %s, loss: %s"%(curr_W, curr_b, curr_loss))
